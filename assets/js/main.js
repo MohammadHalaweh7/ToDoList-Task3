d4922e150739519ba0641345ffd09563b55a5b63
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-04-25 21:12:23 +0300

    Create update icon and add pop-up when update and add task

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@ var tasksArray = [];
 var deleteIcon = document.querySelector(".deleteIcon");
 var deleteAllButton = document.querySelector("#deleteAllButton");
 
+var currentIndex;
+
 // to check if the local storage empty or not
 if (localStorage.getItem("tasksList") == null) {
   tasksArray = [];
@@ -21,18 +23,31 @@ if (localStorage.getItem("tasksList") == null) {
 // ----------------------------------------------------------------------------------------------------------
 // Add Task Button
 addTaskButton.onclick = function () {
-  addTask();
+  if (addTaskButton.innerHTML == "Add-Task") {
+    addTask();
+    // sweet alert
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "The task has been added successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    updateTask();
+    // sweet alert
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "The task has been updated successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    document.getElementById("addTaskButton").innerHTML = "Add-Task";
+  }
+
   displayData();
   clear();
-
-  // sweet alert
-  Swal.fire({
-    position: "top-center",
-    icon: "success",
-    title: "The task has been added successfully",
-    showConfirmButton: false,
-    timer: 1500,
-  });
 };
 // add task function
 function addTask() {
@@ -56,8 +71,10 @@ function displayData() {
           </div>
       
           <div class="icons">
-          <a onclick="deleteTask(${i})" class="deleteIcon mb-3 d-block text-danger fs-5" href="#"><i class="fa fa-trash" aria-hidden="true"></i></a>
+          <a onclick="deleteTask(${i})" class="deleteIcon d-block text-danger fs-5" href="#"><i class="fa fa-trash" aria-hidden="true"></i></a>
+          <a onclick="getTaskData(${i})" class="readIcon d-block text-warning fs-5" href="#"><i class="fa fa-id-card" aria-hidden="true"></i></a>
           <a class="readIcon d-block text-success fs-5" href="#"><i class="fa fa-check-circle" aria-hidden="true"></i></a>
+
           </div>
       
           </div>
@@ -131,7 +148,8 @@ function search(e) {
           </div>
       
           <div class="icons">
-          <a onclick="deleteTask(${i})" class="deleteIcon mb-3 d-block text-danger fs-5" href="#"><i class="fa fa-trash" aria-hidden="true"></i></a>
+          <a onclick="deleteTask(${i})" class="deleteIcon d-block text-danger fs-5" href="#"><i class="fa fa-trash" aria-hidden="true"></i></a>
+          <a onclick="getTaskData(${i})" class="readIcon d-block text-warning fs-5" href="#"><i class="fa fa-id-card" aria-hidden="true"></i></a>
           <a class="readIcon d-block text-success fs-5" href="#"><i class="fa fa-check-circle" aria-hidden="true"></i></a>
           </div>
       
@@ -141,4 +159,22 @@ function search(e) {
     }
   }
   tasksData.innerHTML = result;
+}
+
+function getTaskData(index) {
+  taskName.value = tasksArray[index].name;
+  assignee.value = tasksArray[index].assignee;
+  addTaskButton.innerHTML = "Update Task";
+  currentIndex = index;
+}
+
+function updateTask() {
+  var taskObj = {
+    name: taskName.value,
+    assignee: assignee.value,
+  };
+  tasksArray[currentIndex].name = taskObj.name;
+  tasksArray[currentIndex].assignee = taskObj.assignee;
+
+  localStorage.setItem("tasksList", JSON.stringify(tasksArray));
 }
